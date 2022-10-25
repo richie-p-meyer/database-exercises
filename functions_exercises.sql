50ca57af2b4e3437fc191342329e2021ab360fa3
Richard Meyer <richie.p.meyer@gmail.com>
2022-10-25 16:46:27 -0500

    corrected an error in function

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -21,4 +21,4 @@ SELECT MIN(salary) as min_salary, MAX(salary) AS max_salary
 FROM salaries;
 
 -- Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born.
-SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 4), SUBSTR(last_name, 1,4),'_',SUBSTR(birth_date, 6, 2),SUBSTR(birth_date, 3, 2))) FROM employees;
+SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1,4),'_',SUBSTR(birth_date, 6, 2),SUBSTR(birth_date, 3, 2))) FROM employees;
